7c1fe7df982599045150a4d56e61732eec0b2658
yash91989201 <yashraj6370@gmail.com>
2024-07-06 17:23:03 +0530

    temp

---
+++ src/app/api/test/route.ts
@@ -0,0 +1,8 @@
+import { headers } from "next/headers";
+import { type NextRequest, userAgent } from "next/server";
+
+export function GET(request: NextRequest) {
+  console.log(request.ip)
+
+  return Response.json({ status: "ok" })
+}
