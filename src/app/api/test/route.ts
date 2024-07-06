1dc139112759fee0bcb3e6e86862fe681ed17640
yash91989201 <yashraj6370@gmail.com>
2024-07-06 17:40:23 +0530

    temp

--- src/app/api/test/route.ts
+++
@@ -1,8 +0,0 @@
-import { headers } from "next/headers";
-import { type NextRequest, userAgent } from "next/server";
-
-export function GET(request: NextRequest) {
-  console.log(request.ip)
-
-  return Response.json({ status: "ok", ip: request.ip ?? "localhost" })
-}
